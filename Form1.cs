c5342810d4be80cc5d2d3d0c5c85ec13ed551457
Kacper <Kacper@U-MRT92U41E6S6P>
2023-11-05 18:48:12 +0100

    do zrobienia: merge i quick

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ namespace AiSD_IO_gr3_1
 {
     public partial class Form1 : Form
     {
+        int[] tablica;
         public Form1()
         {
             InitializeComponent();
@@ -13,15 +14,14 @@ namespace AiSD_IO_gr3_1
         {
 
         }
-        int[] tab;
-        void bubble(int[] tab)//select sort drugie
+        void bubble(int[] tab)//
         {
             int tmp; bool cbz = false;
 
             do
             {
                 cbz = false;
-                for (int i = 0; i < tab.Length - 1; i++)
+                for (int i = 0; i < tab.Length - 1; i++)//zamienia liczbe po lewej z liczba poprawej jesli jest wieksza i robi to az do momentu gdy nie bedzie zadnych zmian
                 {
 
                     if (tab[i] > tab[i + 1])
@@ -36,7 +36,7 @@ namespace AiSD_IO_gr3_1
             } while (cbz);
 
         }
-        void select_sort(int[] tab)
+        void select_sort(int[] tab)//szukamy najni¿szej i zamieniamy z pierwszym aktualnym indexem
         {
             int tmp;
             for (int i = 0; i < tab.Length - 1; i++)
@@ -50,6 +50,21 @@ namespace AiSD_IO_gr3_1
                 tmp = tab[min];
                 tab[min] = tab[i];
                 tab[i] = tmp;
+            }
+        }
+
+        void insert_sort(int[] tab)
+        {
+            for(int i = 1; i < tab.Length; i++)
+            {
+                int key = tab[i];
+                int j = i - 1;
+                while (j >= 0&&tab[j] > key)
+                {
+                    tab[j+1] = tab[j];
+                    j--;
+                }
+                tab[j+1]= key;
             }
         }
 
@@ -79,33 +94,82 @@ namespace AiSD_IO_gr3_1
             {
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
-                select_sort(tab);
+                select_sort(tablica);
                 stopwatch.Stop();
                 TimeSpan czasTrwania = stopwatch.Elapsed;
                 label1.Text = czasTrwania.TotalMilliseconds + "ms";
-                textBoxpokaz.Text = string.Join('_', tab);
+                textBoxpokaz.Text = string.Join('_', tablica);
+                label4.Text = "";
             }
 
         }
 
         private void buttonsb_Click(object sender, EventArgs e)
         {
-            string[] liczbyStr = textBoxwpisz.Text.Split('_');
-            int[] liczby = new int[liczbyStr.Length];
-            for (int i = 0; i < liczbyStr.Length; i++)
+            if (!checkbox.Checked)
             {
-                if (int.TryParse(liczbyStr[i], out int val))
+                string[] liczbyStr = textBoxwpisz.Text.Split('_');
+                int[] liczby = new int[liczbyStr.Length];
+                for (int i = 0; i < liczbyStr.Length; i++)
                 {
-                    liczby[i] = val;
+                    if (int.TryParse(liczbyStr[i], out int val))
+                    {
+                        liczby[i] = val;
+                    }
                 }
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                bubble(liczby);
+                stopwatch.Stop();
+                TimeSpan czasTrwania = stopwatch.Elapsed;
+                label1.Text = czasTrwania.TotalMilliseconds + "ms";
+                textBoxpokaz.Text = string.Join('_', liczby);
             }
-            bubble(liczby);
-            textBoxpokaz.Text = string.Join('_', liczby);
+            else
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                bubble(tablica);
+                stopwatch.Stop();
+                TimeSpan czasTrwania = stopwatch.Elapsed;
+                label1.Text = czasTrwania.TotalMilliseconds + "ms";
+                textBoxpokaz.Text = string.Join('_', tablica);
+                label4.Text = "";
+            }
         }
 
         private void buttonsi_Click(object sender, EventArgs e)
         {
-
+            if (!checkbox.Checked)
+            {
+                string[] liczbyStr = textBoxwpisz.Text.Split('_');
+                int[] liczby = new int[liczbyStr.Length];
+                for (int i = 0; i < liczbyStr.Length; i++)
+                {
+                    if (int.TryParse(liczbyStr[i], out int val))
+                    {
+                        liczby[i] = val;
+                    }
+                }
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                insert_sort(liczby);
+                stopwatch.Stop();
+                TimeSpan czasTrwania = stopwatch.Elapsed;
+                label1.Text = czasTrwania.TotalMilliseconds + "ms";
+                textBoxpokaz.Text = string.Join('_', liczby);
+            }
+            else
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                insert_sort(tablica);
+                stopwatch.Stop();
+                TimeSpan czasTrwania = stopwatch.Elapsed;
+                label1.Text = czasTrwania.TotalMilliseconds + "ms";
+                textBoxpokaz.Text = string.Join('_', tablica);
+                label4.Text = "";
+            }
         }
 
         private void buttonsm_Click(object sender, EventArgs e)
@@ -144,10 +208,10 @@ namespace AiSD_IO_gr3_1
         {
             int n = (int)numericUpDown1.Value;
             Random random = new Random();
-            tab = new int[n];
+            tablica = new int[n];
             for (int i = 0; i < n; i++)
             {
-                tab[i] = random.Next(0, 100);
+                tablica[i] = random.Next(0, 100);
             }
             label4.Text = "Wygenerowano Liczby";
             label4.Visible = true;
